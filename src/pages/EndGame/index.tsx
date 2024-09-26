4386f066203cab49f42ae26f192f126e8a013ba1
Pedro Paulo Oliveira <119642579+pepepepu@users.noreply.github.com>
2024-09-26 12:29:50 +0000

    EndGame Screen Improvement

--- src/pages/EndGame/index.tsx
+++ src/pages/EndGame/index.tsx
@@ -4,9 +4,11 @@ import { Div, Paragraph, Button, PlantBoxCarousel } from "../../components";
 import { useGame } from "../../context/GameContext";
 import colors from "../../styles/colors";
 import Confetti from "react-confetti";
+import getDeviceType from "../../hooks/getDeviceType";
 
 const EndGameScreen: React.FC = () => {
   const { team1Score, setTeam1Score } = useGame();
+  const deviceType = getDeviceType();
   const navigate = useNavigate();
 
   const { innerWidth: width, innerHeight: height } = window;
@@ -27,15 +29,15 @@ const EndGameScreen: React.FC = () => {
       backgroundColor={colors.branco}
       direction="column"
       justify="space-between"
-      gap={40}
+      gap={deviceType === "smartphone" ? 30 : 40}
     >
       <Confetti width={width} height={height} />
       <PlantBoxCarousel />
-      <Div gap={40}  width={"100%"}>
+      <Div gap={deviceType === "smartphone" ? 30 : 40} width={"100%"}>
         <Div>
           <Paragraph
             textShadow="2px 2px 4px rgba(0, 0, 0, 0.149)"
-            fontSize={60}
+            fontSize={deviceType === "smartphone" ? 45 : 60}
             fontWeight={700}
             margin={0}
             color={colors.preto}
@@ -44,7 +46,7 @@ const EndGameScreen: React.FC = () => {
           </Paragraph>
           <Paragraph
             textShadow="2px 2px 4px rgba(0, 0, 0, 0.149)"
-            fontSize={30}
+            fontSize={deviceType === "smartphone" ? 20 : 30}
             fontWeight={400}
             margin={0}
             color={colors.preto}
@@ -52,11 +54,16 @@ const EndGameScreen: React.FC = () => {
             Obrigado por participar!
           </Paragraph>
         </Div>
-        <Div  height={"20%"} width={"70%"} direction="row" gap={30}>
+        <Div
+          width={"70%"}
+          direction={deviceType === "smartphone" ? "column" : "row"}
+          gap={deviceType === "smartphone" ? 10 : 30}
+        >
           <Button
+            width={deviceType === "smartphone" ? "80%" : (deviceType === "tablet" ? "45%" : "20%")}
             margin={0}
+            padding={deviceType === "smartphone" ? "10px 0px" : "20px 0px"}
             borderRadius={30}
-            padding={"1.5% 4%"}
             onClick={() => handleNavigateNewGame()}
             backgroundColor={colors.castanha}
             hoverBackgroundColor="#4d3d1e"
@@ -65,14 +72,18 @@ const EndGameScreen: React.FC = () => {
             hoverScale={1.1}
             animationDuration="0.4s"
           >
-            <Paragraph fontWeight={700} fontSize={25}>
+            <Paragraph
+              fontWeight={700}
+              fontSize={deviceType === "smartphone" ? 20 : 25}
+            >
               Novo jogo
             </Paragraph>
           </Button>
           <Button
+            width={deviceType === "smartphone" ? "80%" : (deviceType === "tablet" ? "45%" : "20%")}
             margin={0}
+            padding={deviceType === "smartphone" ? "10px 0px" : "20px 0px"}
             borderRadius={30}
-            padding={"1.5% 4%"}
             onClick={() => handleNavigateResults()}
             backgroundColor={colors.castanha}
             hoverBackgroundColor="#4d3d1e"
@@ -81,7 +92,10 @@ const EndGameScreen: React.FC = () => {
             hoverScale={1.1}
             animationDuration="0.4s"
           >
-            <Paragraph fontWeight={700} fontSize={25}>
+            <Paragraph
+              fontWeight={700}
+              fontSize={deviceType === "smartphone" ? 20 : 25}
+            >
               Ver resultados
             </Paragraph>
           </Button>
